f781e3c2fd5b46069b5316d54830caf1a27927a5
SaffiSardar <saffisardar2012@gmail.com>
2025-06-17 01:39:26 +0500

    deleted datasetinstall.py

--- datasetinstall.py
+++
@@ -1,7 +0,0 @@
-#!C:\Users\Dell\Documents\Visual Studio Code\AI project\env\Scripts\python.exe
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("C:\Users\Dell\Documents\Visual Studio Code\AI project")
-
-print("Path to dataset files:", path)
